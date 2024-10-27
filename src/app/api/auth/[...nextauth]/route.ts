545cc6bfbe4408dae4f65abf5f4d651250ad4cec
writetochetanpandey <writetochetanpandey@gmail.com>
2024-10-27 17:34:27 +0800

    Optimizations at Sun10-27-2024_17-34-27.17 ='

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth"
+import NextAuth, { AuthOptions } from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import CredentialsProvider from "next-auth/providers/credentials"
 import bcrypt from "bcrypt"
@@ -6,7 +6,7 @@ import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient()
 
-const handler = NextAuth({
+export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     CredentialsProvider({
@@ -68,6 +68,8 @@ const handler = NextAuth({
   pages: {
     signIn: "/login",
   },
-})
+}
+
+const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,25 +1,27 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 // next-auth.d.ts
-import NextAuth from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth"
 
 declare module "next-auth" {
-  interface Session {
-    user: {
-      id: string;
-      role: string;
-    };
-  }
-}
-import NextAuth from "next-auth"
-
-declare module "next-auth" {
-  interface Session {
+  interface Session extends DefaultSession {
     user: {
       id: string
       name: string
       email: string
       role: string
-    }
+    } & DefaultSession["user"]
   }
+
+  interface User {
+    role: UserRole
+  }
+}
+
+export enum UserRole {
+  EMPLOYEE = "EMPLOYEE",
+  MANAGER = "MANAGER",
+  SUPERVISOR = "SUPERVISOR",
+  ADMIN = "ADMIN",
+  SUPERADMIN = "SUPERADMIN"
 }
