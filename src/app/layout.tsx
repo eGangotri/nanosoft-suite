6719539e3cf20f2aaed1b8d2d2c5ebb38fa204dc
writetochetanpandey <writetochetanpandey@gmail.com>
2024-11-06 18:16:05 +0800

    Optimizations at Wed11-06-2024_18-16-04.90 ='

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+      <link rel="icon" href="/favicon.ico" />
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
       </head>
       <body className={inter.className}>
