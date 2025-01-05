6da0112ec417ae5f2c428fb89d1a0a0d228b3d21
writetochetanpandey <writetochetanpandey@gmail.com>
2025-01-05 14:23:43 +0700

    -

--- src/globalConstants.ts
+++ src/globalConstants.ts
@@ -5,9 +5,18 @@ export const GLOBAL_CONSTS = {
         return _prefix.length > 0 ? `${_prefix} - ${GLOBAL_CONSTS.title}` : GLOBAL_CONSTS.title;
     }
 }
-export const NANOSOFT_ROLES = {
+
+export const NANOSOFT_ADMIN_ROLES = {
     ADMIN: 'ADMIN',
-    EMPLOYEE: 'EMPLOYEE',
-    SUPERVISOR: 'SUPERVISON',
+    SUPERVISOR: 'SUPERVISOR',
     SUPERADMIN: 'SUPERADMIN'
+}
+
+export const NANOSOFT_NON_ADMIN_ROLES = {
+    EMPLOYEE: 'EMPLOYEE',
+}
+
+export const NANOSOFT_ROLES = {
+    ...NANOSOFT_ADMIN_ROLES,
+    ...NANOSOFT_NON_ADMIN_ROLES
 }
