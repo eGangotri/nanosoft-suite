d2760da7293ab096f36d93a261b220c00c18f313
writetochetanpandey <writetochetanpandey@gmail.com>
2025-01-21 23:52:24 +0700

    Optimizations at Tue01-21-2025_23-52-24.29 ='

--- src/app/api/roles/route.ts
+++ src/app/api/roles/route.ts
@@ -1,28 +1,31 @@
 import { NextResponse } from "next/server"
 import { PrismaClient } from "@prisma/client"
 import { getServerSessionWithDefaultAuthOptions } from "../auth/[...nextauth]/route";
+import { isAdmin, isAdminOrSuperAdmin, isSuperAdmin } from "@/utils/utils";
+import { is } from "date-fns/locale";
 
 const prisma = new PrismaClient()
 
 export async function GET(request: Request) {
     const session = await getServerSessionWithDefaultAuthOptions();
-    if (!session || !session.user) {
+    if (!session || !session.user || !isAdminOrSuperAdmin(session.user.role)) {
         return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
     }
 
-    const userId = session.user.id;
-    const tenantId = session.user.tenantId;
     try {
-        const userRoles = await prisma.userRole.findMany({
-            where: {
-                tenantId: tenantId,
-            },
-            include: {
-                Role: true,
-            },
+        const userRoles = await prisma.role.findMany({
+            select: {
+                id: true,
+                name: true
+            }
         })
 
-        return NextResponse.json(userRoles)
+        if (isAdmin(session.user.role)) {
+            return NextResponse.json(userRoles.filter(role => !isSuperAdmin(role.name)))
+        }
+        else {
+            return NextResponse.json(userRoles)
+        }
     } catch (error) {
         console.error("Error fetching user roles:", error)
         return NextResponse.json({ error: "Internal Server Error" }, { status: 500 })
